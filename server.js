71f152faf44dfc0f3d824cb017f943913ca148bf
Gargi Jadaun <128454603+Gargijadaun@users.noreply.github.com>
2025-04-14 13:38:35 +0530

    r

--- server.js
+++ server.js
@@ -17,15 +17,17 @@ if (fs.existsSync(usersFile)) {
 }
 
 app.post('/register', (req, res) => {
-    const { email, password } = req.body;
+    const { name, email, phone, password } = req.body;
+
     if (users[email]) {
         return res.status(400).json({ message: 'User already exists' });
     }
 
     const code = Math.floor(100000 + Math.random() * 900000).toString(); // Generate 6-digit code
 
-    users[email] = { password, code };
+    users[email] = { name, phone, password, code };
     fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
+
     res.json({ message: 'Registered successfully', code });
 });
 
@@ -50,3 +52,14 @@ app.post('/vr-login', (req, res) => {
 app.listen(PORT, () => {
     console.log(`🚀 TFG WEB is running at http://localhost:${PORT}`);
 });
+
+app.get('/get-users', (req, res) => {
+    if (!fs.existsSync(usersFile)) {
+        return res.status(404).json({ message: 'User data not found' });
+    }
+
+    const data = fs.readFileSync(usersFile, 'utf8');
+    const allUsers = JSON.parse(data);
+    res.json(allUsers);
+});
+
